0e901f873575b22b651ccedf5f4b2975a537ceec
aman jishtu <aman_jishtu@yahoo.com>
2021-08-30 09:12:11 -0400

    added java 1

---
+++ Classwork/HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author amanjishtu
+ * email: aman_jishtu@yahoo.com
+ * date: 29/08/2021
+ * purpose: My first Hello World program in an IDE
+ */
+// Use Your Group Id value here if it is different from com.mthree
+package com.amanjishtu.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Aman!");
+    
+    }
+    
+}
